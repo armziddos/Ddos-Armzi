1b191b5f7ea9c8a72d6d11a0f38f78336d6cf4d2
armziddos <scamlisa697@gmail.com>
2026-04-01 00:22:47 +0530

    Refactor and restore m.py with bot functionality

--- m.py
+++ m.py
@@ -48,10 +48,10 @@ allowed_user_ids = read_users()
 
 # Function to log command to the file
 def log_command(user_id, target, port, time):
-    admin_id = ["5935306519"]
+    admin_id = ["6158419745"]
     user_info = bot.get_chat(user_id)
     if user_info.username:
-        username = "@" + user_info.username
+        username = "@Tenziikom12344" + user_info.username
     else:
         username = f"UserID: {user_id}"
     
